af43679f6983241f284a1249f25783dd4256e607
Liander Souza <liandervinicius13@gmail.com>
2024-05-19 23:49:47 -0300

    estilizando sections

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,6 +1,18 @@
-const botaoMenuMobile = document.querySelector('.menu-mobile')
-const menuMobile = document.querySelector('.menu-navegacao')
 
-botaoMenuMobile.addEventListener('click', () => {
-    menuMobile.classList.add('menu-ativo')
-})
+
+
+function obterLarguraPagina() {
+    const largura = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    return largura;
+}
+
+window.addEventListener('resize', () => {
+    const btn_comprar = document.querySelector('.btn-comprar')
+    const largura = obterLarguraPagina();
+    
+    if (largura <= 500) {
+        btn_comprar.textContent = 'Comprar'
+    } else {
+        btn_comprar.textContent = 'Adicionar ao carrinho'
+    }
+});
